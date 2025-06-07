f79dd62736978eee7274e2e2f4d171b13a1880a4
Johny Ho <johny@thoughtbot.com>
2025-06-07 17:20:04 -0400

    Remove render_to
    Like update_to, render_to is kind of the same thing. The difference here is
    that render_to in turbo allows you to perform multiple actions (append,
    replace, etc. at the same time). But superglue's fragment response should also
    be capable of that too (in the near future), so a simple replace_to should suffice,
    and again, i think i'll eventually rename replace_to to something more generate,
    like save_to or something.

--- app/jobs/superglue/streams/broadcast_job.rb
+++
@@ -1,7 +0,0 @@
-class Turbo::Streams::BroadcastJob < ActiveJob::Base
-  discard_on ActiveJob::DeserializationError
-
-  def perform(stream, **rendering)
-    Turbo::StreamsChannel.broadcast_render_to stream, **rendering
-  end
-end
