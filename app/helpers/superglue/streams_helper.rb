60249d6525aa121622890c082176340f007d44b7
Johny Ho <johny@thoughtbot.com>
2025-06-23 23:02:31 -0400

    Add fragment_id view helper

--- app/helpers/superglue/streams_helper.rb
+++ app/helpers/superglue/streams_helper.rb
@@ -1,10 +1,19 @@
 module Superglue::StreamsHelper
-  # todo: add fragment_id
   def stream_from_props(*streamables, **attributes)
     raise ArgumentError, "streamables can't be blank" unless streamables.any?(&:present?)
     attributes[:channel] = attributes[:channel]&.to_s || "Superglue::StreamsChannel"
     attributes[:signed_stream_name] = Superglue::StreamsChannel.signed_stream_name(streamables)
 
     attributes
+  end
+
+  def fragment_id(value)
+    if value.respond_to?(:to_key)
+      ActionView::RecordIdentifier.dom_id(value)
+    elsif value.respond_to?(:broadcast_fragment_default)
+      value.broadcast_fragment_default
+    else
+      value.to_s
+    end
   end
 end
