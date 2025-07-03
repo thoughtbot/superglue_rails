22b42b210bcdad099390e10c790b6f174dcf0d16
Johny Ho <johny@thoughtbot.com>
2025-07-02 20:30:21 -0400

    Add support for stream response
    This is equivalent to Turbostream's stream responses. Here's create example:
    
    Turbostreams:
    
    ```
      def create
        message = Message.create!(params.require(:message).permit(:content))
    
        respond_to do |format|
          format.turbo_stream {render locals: {message: message} }
          # For browsers without JS
          format.html { redirect_to messages_url }
        end
      end
    ```
    and in `create.turbo_stream.erb`
    
    ```
    <%= turbo_stream.prepend "messages", @message%>
    ```
    
    in Superglue this would be
    
    ```
      def create
        message = Message.create!(params.require(:message).permit(:content))
    
        respond_to do |format|
          format.json { render layout: 'stream', locals: {message: message}}
          # For browsers without JS
          format.html { redirect_to messages_url }
        end
      end
    ```
    
    and in `create.json.props`
    
    ```
      broadcast_prepend_props(model: @message, fragment: 'messages')
    ```
    
    Notice that we have to use a `stream` layout, that will be included in every generated
    install of superglue_rails.

--- app/helpers/superglue/streams_helper.rb
+++ app/helpers/superglue/streams_helper.rb
@@ -16,4 +16,51 @@ module Superglue::StreamsHelper
       value.to_s
     end
   end
+
+  def broadcast_prepend_props(model: nil, fragment: nil, save_as: nil, options: {}, **rendering)
+    if save_as
+      options[:saveAs] ||= fragment_id(save_as)
+    end
+
+    broadcast_action_props(action: "prepend", model:, fragment:, options:, **rendering)
+  end
+
+  def broadcast_append_props(model: nil, fragment: nil, save_as: nil, options: {}, **rendering)
+    if save_as
+      options[:saveAs] ||= fragment_id(save_as)
+    end
+
+    broadcast_action_props(action: "append", model:, fragment:, options:, **rendering)
+  end
+
+  def broadcast_save_props(model: nil, partial: nil, fragment: nil, options: {}, **rendering)
+    broadcast_action_props(action: "save", model:, fragment:, options:, **rendering)
+  end
+
+  def broadcast_action_props(action:, partial: nil, model: nil, fragment: nil, options: {}, **rendering)
+    if model
+      fragment = model.broadcast_fragment_default if !fragment
+
+      if model.respond_to?(:to_partial_path)
+        rendering[:locals] = (rendering[:locals] || {}).reverse_merge(model.model_name.element.to_sym => model).compact
+        partial ||= model.to_partial_path
+      end
+    end
+
+    fragment = fragment_id(fragment)
+
+    if !partial
+      raise StandardError, "A partial is needed to render a stream"
+    end
+
+    json = instance_variable_get(:@__json)
+
+    json.child! do
+      json.fragmentKeys [fragment]
+      json.action action
+      json.options(options)
+      json.data(partial: [partial, rendering]) do
+      end
+    end
+  end
 end
